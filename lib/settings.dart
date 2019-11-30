5dc7ede1ed1bae3367f48dd9817ebdb907c78dc7
Ulisses Chaves <ulisseschaves8@gmail.com>
2019-11-29 23:58:12 -0300

    Pequenas modificações

--- lib/settings.dart
+++ lib/settings.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'models/user.model.dart';
 
 class Settings {
-  static String apiUrl = "http://app.prae.ufrpe.br/rectec/api/";
+  static String apiUrl = "";
   static User user;
-  static String username = 'ulisseschaves8@gmail.com'; 
-  static String password = 'franguli';
+  static String username = ''; 
+  static String password = '';
   static var basicAuth = {
       'Authorization': 'Basic ' + base64Encode(utf8.encode('$username:$password'))
     };
